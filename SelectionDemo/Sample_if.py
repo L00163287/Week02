8606a64086a4202e48e6aca8813f441c8699c29f
Bubashan <bubashanb1@gmail.com>
2021-09-27 10:19:17 +0100

    Add If Example

---
+++ SelectionDemo/Sample_if.py
@@ -0,0 +1,15 @@
+"""
+#
+# @File         : Sample_if.py
+# @Created      : 2021-09-27 10:17
+# @Author       : Bubashankushan B
+# @Version      : v1.0.0
+# @Licensing    : 
+#
+# @Description  : if Example
+#
+"""
+
+if __name__ == '__main__':
+    grade = 50
+    print(grade)
