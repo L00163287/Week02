c6c0042b78aabda2780f89535e4d5793eca19bc1
Bubashan <bubashanb1@gmail.com>
2021-09-27 10:16:07 +0100

    Create Project

---
+++ LoopDemo/__init__.py
@@ -0,0 +1,11 @@
+"""
+#
+# @File         : __init__.py.py
+# @Created      : 2021-09-27 10:14
+# @Author       : Bubashankushan B
+# @Version      : v1.0.0
+# @Licensing    : 
+#
+# @Description  :
+#
+"""
