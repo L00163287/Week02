b8dadf732d8461b5de10a0da18c8584f20f86e11
Bubashan <bubashanb1@gmail.com>
2021-10-04 21:52:38 +0100

    Question 03 answers added

--- Tutorial/Question03.py
+++ Tutorial/Question03.py
@@ -11,5 +11,20 @@
 """
 
 if __name__ == "__main__":
+    upperCaseLetters = 0
+    lowerCaseLetters = 0
+    numbers = 0
 
-    userString = input("Enter a string")
+    userString = input("Enter a string  :")  # Get user input from user
+
+    for i in userString:  # iterate for each letter in the user Input
+        if 'A' <= i <= 'Z':  # check for upper case letters and increase count
+            upperCaseLetters += 1
+        elif 'a' <= i <= 'z':  # check for lower case letters and increase count
+            lowerCaseLetters += 1
+        else:  # check for number and increase count
+            numbers += 1
+
+    print('Letter Counts in user string : {} =>  \t'.format(userString))  # Printing the character counts
+    print("Uppercase Letters : {} \nLowercase letters : {}" .format(upperCaseLetters, lowerCaseLetters))
+    print('Numbers \t : {}'.format(numbers))
